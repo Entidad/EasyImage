c10ae57066a3f0fe99beba308bd9c2d49aba9085
Ockert van Schalkwyk <ockert.vanschalkwyk@entidad.io>
2024-02-20 17:39:40 +0200

    added test project
    Signed-off-by: Ockert van Schalkwyk <ockert.vanschalkwyk@entidad.io>

---
+++ test/javasource/system/UserActionsRegistrar.java
@@ -0,0 +1,20 @@
+package system;
+
+import com.mendix.core.actionmanagement.IActionRegistrator;
+
+public class UserActionsRegistrar
+{
+  public void registerActions(IActionRegistrator registrator)
+  {
+    registrator.bundleComponentLoaded();
+    registrator.registerUserAction(encryption.actions.DecryptString.class);
+    registrator.registerUserAction(encryption.actions.EncryptString.class);
+    registrator.registerUserAction(encryption.actions.GeneratePGPKeyRing.class);
+    registrator.registerUserAction(encryption.actions.PGPDecryptDocument.class);
+    registrator.registerUserAction(encryption.actions.PGPEncryptDocument.class);
+    registrator.registerUserAction(encryption.actions.ValidatePrivateKeyRing.class);
+    registrator.registerUserAction(main.actions.ja_get_admin_user_name.class);
+    registrator.registerUserAction(main.actions.ja_get_admin_user_rolename.class);
+    registrator.registerUserAction(system.actions.VerifyPassword.class);
+  }
+}
